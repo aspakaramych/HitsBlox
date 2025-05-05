c1abbf22628457ca75dea9d099cea0ca9ce22271
mkenopsia <gxkurro@gmail.com>
2025-05-05 21:05:10 +0700

    transfer changes dimbl

--- App/lib/main.dart
+++ App/lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
           ),
       ),
       routes: {
-        '/': (context) => HomeScreen(),
+        '/': (context) => mainScreen(),
         '/test-page': (context) => TestScreen()
       },
       // home: const HomeScreen(),
