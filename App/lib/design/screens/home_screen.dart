6c15d187fd55fefd83227427bd266d96ce766853
dmitryLem <dlemesonok0@gmail.com>
2025-05-05 21:09:53 +0700

    fixed mainpage

--- App/lib/design/screens/home_screen.dart
+++ App/lib/design/screens/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'test_screen.dart';
 import 'package:app/design/widgets/widgets.dart';
-import 'package:app/design/theme/colors.dart';
 
 class mainScreen extends StatefulWidget {
   const mainScreen({super.key});
