8d9ac0a42d0fe2b14dccf479553e63fedeedcfb0
dmitryLem <dlemesonok0@gmail.com>
2025-05-11 20:47:27 +0700

    design bottom_bar.dart

--- App/lib/design/widgets/bottom_bar.dart
+++ App/lib/design/widgets/bottom_bar.dart
@@ -4,6 +4,8 @@ class BottomBar extends StatelessWidget {
   final VoidCallback onTerminalPressed;
   final VoidCallback onAddPressed;
 
+  final String shape = "M375 62.1514C375 67.6742 370.523 72.1514 365 72.1514H15C9.47715 72.1514 5 67.6742 5 62.1514V13C5 7.47715 9.47715 3 15 3H141.654C145.577 3 149.056 5.34976 151.162 8.65935C159.324 21.4884 173.668 30 190 30C206.332 30 220.676 21.4884 228.838 8.65935C230.944 5.34976 234.423 3 238.346 3H365C370.523 3 375 7.47715 375 13V62.1514Z";
+
   const BottomBar({
     super.key,
     required this.onTerminalPressed,
@@ -13,75 +15,186 @@ class BottomBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 20),
-      margin: EdgeInsets.only(bottom: 20),
+      height: 165,
+      color: Colors.transparent,
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
       width: double.infinity,
-      height: 70,
-      child: Container(
-        padding: EdgeInsets.all(8),
-        decoration: BoxDecoration(
-          color: AppColors.primary,
-          borderRadius: BorderRadius.all(Radius.circular(25)),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            IconButton(
-              icon: SvgPicture.asset(
-                'lib/design/assets/icons/home.svg',
-                  width: 40,
-                  height: 40,
-                  colorFilter: ColorFilter.mode(Colors.black, BlendMode.srcIn),
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          PhysicalModel(
+            elevation: 10,
+            color: Colors.transparent,
+            shadowColor: Colors.black.withOpacity(0.01),
+            clipBehavior: Clip.none,
+            shape: BoxShape.rectangle,
+            child: Container(
+              // padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              child: CustomPaint(
+                painter: BottomBarShadowPainter(shape),
+                child: SizedBox(
+                  height: 80,
+                  width: MediaQuery.of(context).size.width + 40,
                 ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => HomeScreen()),
-                );
-              },
-            ),
-            IconButton(
-              icon: SvgPicture.asset(
-                'lib/design/assets/icons/terminal.svg',
-                width: 40,
-                height: 40,
-                colorFilter: ColorFilter.mode(Colors.black, BlendMode.srcIn),
               ),
-              onPressed: onTerminalPressed,
             ),
-            IconButton(
-              icon: SvgPicture.asset(
-                'lib/design/assets/icons/add.svg',
-                width: 40,
-                height: 40,
-                colorFilter: ColorFilter.mode(Colors.black, BlendMode.srcIn),
+          ),
+
+          ClipPath(
+            clipper: CustomSvgClipper(shape),
+            child: Container(
+              color: AppColors.primary,
+              padding: EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                    icon: SvgPicture.asset(
+                      'lib/design/assets/icons/home.svg',
+                      width: 40,
+                      height: 40,
+                      colorFilter: ColorFilter.mode(
+                          Colors.black, BlendMode.srcIn),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => HomeScreen()),
+                      );
+                    },
+                  ),
+                  IconButton(
+                    icon: SvgPicture.asset(
+                      'lib/design/assets/icons/terminal.svg',
+                      width: 40,
+                      height: 40,
+                      colorFilter: ColorFilter.mode(
+                          Colors.black, BlendMode.srcIn),
+                    ),
+                    onPressed: onTerminalPressed,
+                  ),
+                  // IconButton(
+                  //   icon: SvgPicture.asset(
+                  //     'lib/design/assets/icons/add.svg',
+                  //     width: 40,
+                  //     height: 40,
+                  //     colorFilter: ColorFilter.mode(
+                  //         Colors.black, BlendMode.srcIn),
+                  //   ),
+                  //   onPressed: onAddPressed,
+                  // ),
+                  SizedBox(width: 10,),
+                  IconButton(
+                    icon: SvgPicture.asset(
+                      'lib/design/assets/icons/download.svg',
+                      width: 40,
+                      height: 40,
+                      colorFilter: ColorFilter.mode(
+                          Colors.black, BlendMode.srcIn),
+                    ),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: SvgPicture.asset(
+                      'lib/design/assets/icons/settings.svg',
+                      width: 40,
+                      height: 40,
+                      colorFilter: ColorFilter.mode(
+                          Colors.black, BlendMode.srcIn),
+                    ),
+                    onPressed: () =>
+                        Navigator.push(
+                          context,
+                          PageRouteBuilder(pageBuilder: (context, animation,
+                              secondaryAnimation) => SettingsScreen()),
+                        ),
+                  ),
+                ],
               ),
-              onPressed: onAddPressed,
             ),
-            IconButton(
-              icon: SvgPicture.asset(
-                'lib/design/assets/icons/download.svg',
-                width: 40,
-                height: 40,
-                colorFilter: ColorFilter.mode(Colors.black, BlendMode.srcIn),
+          ),
+
+          Positioned(
+            bottom: 60,
+            child: Material(
+              elevation: 2,
+              borderRadius: BorderRadius.circular(50),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: AppColors.primary,
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: IconButton(
+                  icon: SvgPicture.asset(
+                    'lib/design/assets/icons/add.svg',
+                    width: 40,
+                    height: 40,
+                    colorFilter:
+                    ColorFilter.mode(Colors.black, BlendMode.srcIn),
+                  ),
+                  onPressed: onAddPressed,
+                ),
               ),
-              onPressed: () {},
             ),
-            IconButton(
-              icon: SvgPicture.asset(
-                'lib/design/assets/icons/settings.svg',
-                width: 40,
-                height: 40,
-                colorFilter: ColorFilter.mode(Colors.black, BlendMode.srcIn),
-              ),
-              onPressed: () => Navigator.push(
-                context,
-                PageRouteBuilder(pageBuilder: (context, animation, secondaryAnimation) => SettingsScreen()),
-              )
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
+}
+
+class CustomSvgClipper extends CustomClipper<Path> {
+  final String svgPath;
+
+  CustomSvgClipper(this.svgPath);
+
+  @override
+  Path getClip(Size size) {
+    Path path = parseSvgPathData(svgPath);
+
+    Rect originalBounds = path.getBounds();
+    double originalWidth = originalBounds.width;
+    double originalHeight = originalBounds.height;
+
+    double scaleX = size.width / originalWidth;
+    double scaleY = size.height / originalHeight;
+
+    Matrix4 matrix = Matrix4.identity()
+      ..scale(scaleX, scaleY)
+      ..translate(-originalBounds.left, -originalBounds.top);
+
+    return path.transform(matrix.storage);
+  }
+
+  @override
+  bool shouldReclip(CustomSvgClipper oldDelegate) => true;
+}
+
+class BottomBarShadowPainter extends CustomPainter {
+  final String svgPath;
+
+  BottomBarShadowPainter(this.svgPath);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Path path = parseSvgPathData(svgPath);
+    Rect originalBounds = path.getBounds();
+    double scaleX = size.width / originalBounds.width;
+    double scaleY = size.height / originalBounds.height;
+
+    Matrix4 matrix = Matrix4.identity()
+      ..scale(scaleX, scaleY)
+      ..translate(-originalBounds.left, -originalBounds.top);
+
+    Path transformedPath = path.transform(matrix.storage);
+
+    final shadowPaint = Paint()
+      ..color = Colors.black.withOpacity(0.2)
+      ..maskFilter = MaskFilter.blur(BlurStyle.normal, 10);
+
+    canvas.drawPath(transformedPath, shadowPaint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
 }
--- App/lib/design/widgets/widgets.dart
+++ App/lib/design/widgets/widgets.dart
@@ -7,6 +7,7 @@ import 'package:app/screens/test_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import '../theme/colors.dart';
+import 'package:path_drawing/path_drawing.dart';
 
 import '../../core/ConsoleService.dart';
 
