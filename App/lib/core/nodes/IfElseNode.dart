e4003c2dc4e06c1a7ee4b36adfc7fa2b18d29655
aspakaramych <vgrig2006@gmail.com>
2025-05-14 21:52:51 +0700

    add ifelse

---
+++ App/lib/core/nodes/IfElseNode.dart
@@ -0,0 +1,24 @@
+import 'dart:ui';
+
+import 'package:app/core/abstracts/Node.dart';
+import 'package:app/core/registry/VariableRegistry.dart';
+
+class IfElseNode extends Node {
+
+  IfElseNode(String this.id, Offset position) : super(position) {
+
+  }
+  @override
+  Future<void> execute(VariableRegistry registry) {
+    // TODO: implement execute
+    throw UnimplementedError();
+  }
+
+  @override
+  final String id;
+
+  @override
+  // TODO: implement title
+  String get title => throw UnimplementedError();
+
+}
